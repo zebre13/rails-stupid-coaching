0b1a094dba059ba889b28a0e13f7f78e14827ff1
Boris <95616326+zebre13@users.noreply.github.com>
2024-01-07 14:13:45 +0100

    Implement and display question and answer logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
