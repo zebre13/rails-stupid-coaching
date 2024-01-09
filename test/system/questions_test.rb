95b6c0b1321e989ad82ee7fd57d2818d2e1eab65
Boris <95616326+zebre13@users.noreply.github.com>
2024-01-09 17:18:38 +0100

    Tests added

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -1,14 +1,32 @@
 require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit questions_url
-  #
-  #   assert_selector "h1", text: "Questions"
-  # end
-
   test "visiting /ask renders the form" do
     visit ask_url
     assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "asking a question yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+  test "saying the right sentence yields satisfaction from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
   end
 end
